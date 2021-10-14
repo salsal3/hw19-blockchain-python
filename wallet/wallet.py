af77b8b9cbcc1d2b696fd92001555e6ec73e41df
salsal3 <salsal3@gmail.com>
2021-10-14 17:38:07 -0400

    Successfully sent ETH transaction. TODO: README

--- wallet/wallet.py
+++ wallet/wallet.py
@@ -7,8 +7,7 @@ from dotenv import load_dotenv
 
 # Load and set environment variables
 load_dotenv('token.env')
-mnemonic = os.getenv("mnemonic")
-btctest_receiver = os.getenv("btctest_receiver")
+mnemonic = os.getenv('mnemonic')
 
 # Import constants.py and necessary functions from bit and web3
 from constants import *
@@ -21,7 +20,7 @@ from web3.middleware import geth_poa_middleware
 from eth_account import Account
 from getpass import getpass
 
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 
@@ -30,22 +29,21 @@ import time
 import sys
 from random import randrange
 from chatter import chatter
-chatter("Testing chatter function..")
-chatter(".........")
-chatter("Testing complete.")
-chatter("------------")
+chatter('Testing chatter function..')
+chatter('.........')
+chatter('Testing complete.')
+chatter('------------')
 
 # Toggle chatter
-chatter("Would you like to disable chatter function for faster text?")
-chatter_io = input("Disable chatter function? (y/n): ")
+chatter('Would you like to disable chatter function for faster text?')
+chatter_io = input('Disable chatter function? (y/n): ')
 if chatter_io.lower() == 'y':
     def chatter(npc_chatter):
         print(npc_chatter)
-    chatter("Chatter is disabled.")
+    chatter('Chatter is disabled.')
 else:
     from chatter import chatter
-    chatter("Chatter is enabled.")
-    pass
+    chatter('Chatter is enabled.')
 
 
 # Create a function called `derive_wallets`
@@ -72,11 +70,11 @@ def derive_wallets(coin, stdout=subprocess.PIPE, shell=True):
     if coin == ETH:
         global eth_wallet
         eth_wallet = json.loads(output)
-        return eth_wallet
+        return json.loads(output)
     elif coin == BTCTEST:
         global btctest_wallet
         btctest_wallet = json.loads(output)
-        return btctest_wallet
+        return json.loads(output)
 
 # Create a dictionary object called coins to store the output from `derive_wallets`.
 coins = {
@@ -86,34 +84,20 @@ coins = {
 print()
 
 
-# Create wallet variables
-eth_account = coins[ETH][0]['address']
-btctest_account = coins[BTCTEST][0]['address']
-
-eth_privkey = coins[ETH][0]['privkey']
-btctest_privkey = coins[BTCTEST][0]['privkey']
-
-
 # Create a function called `priv_key_to_account` that converts privkey strings to account objects.
 def priv_key_to_account(coin, priv_key):
     if coin == ETH:
-        # Create global variables for the accounts
-        global eth_account
-        eth_account = Account.privateKeyToAccount(eth_privkey)
-        return Account.privateKeyToAccount(eth_privkey)
+        return Account.privateKeyToAccount(priv_key)
     elif coin == BTCTEST:
-        global btctest_account
-        btctest_account = PrivateKeyTestnet(btctest_privkey)
-        return PrivateKeyTestnet(btctest_privkey)
+        return PrivateKeyTestnet(priv_key)
 
 
 # Create a function called `create_tx` that creates an unsigned transaction appropriate metadata.
 def create_tx(coin, account, to, amount):
     chatter(f'Creating unsigned {coin} transaction..')
     if coin == ETH:
-        # Convert ETH to WEI
         gasEstimate = w3.eth.estimateGas(
-            {'from': account.address, 'to': to, 'amount': amount}
+            {'from': account.address, 'to': to, 'value': amount}
         )
         return {
             'from': account.address,
@@ -122,42 +106,57 @@ def create_tx(coin, account, to, amount):
             'gasPrice': w3.eth.gasPrice,
             'gas': gasEstimate,
             'nonce': w3.eth.getTransactionCount(account.address),
-            'chainID': w3.eth.chain_id
         }
-    if coin == BTCTEST:
+    elif coin == BTCTEST:
         return PrivateKeyTestnet.prepare_transaction(account.address, [(to, amount, BTC)])
-    chatter(f'Unsigned transaction created.')
 
 
 # Create a function called `send_tx` that calls `create_tx`, signs and sends the transaction.
 def send_tx(coin, account, to, amount):
     if coin == ETH:
         raw_tx = create_tx(coin, account, to, amount)
+        chatter('Unsigned transaction created.')
+        chatter('Sending transaction..')
         signed_tx = account.sign_transaction(raw_tx)
         result = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
-        print(result.hex())
+        chatter('Transaction sent.')
         return result.hex()
     elif coin == BTCTEST:
         raw_tx = create_tx(coin, account, to, amount)
+        chatter('Unsigned transaction created.')
+        chatter('Sending transaction..')
         signed_tx = account.sign_transaction(raw_tx)
+        chatter('Transaction sent.')
         return NetworkAPI.broadcast_tx_testnet(signed_tx)
 
 
-# ETH Transaction
-chatter(f'# Commands to send a transaction using ETH:')
-chatter(f'priv_key_to_account(ETH, eth_privkey)')
-chatter(f'send_tx(ETH, eth_account, eth_receiver, 0.000001)')
+# Create wallet variables
+eth_account = priv_key_to_account(ETH, eth_privkey)
+eth_address = coins[ETH][0]['address']
+eth_privkey = coins[ETH][0]['privkey']
+eth_receiver = '0x26077A0eaA11E133739F070b001D28267264D24A'
+btctest_account = priv_key_to_account(BTCTEST, btctest_privkey)
+btctest_address = coins[BTCTEST][0]['address']
+btctest_privkey = coins[BTCTEST][0]['privkey']
+btctest_receiver = 'mnLfB25RMps3ABSqK6orbwQDQdMhC1FvS2'
 
-# BTC Transaction
-chatter(f'# Commands to send a transaction using BTCTEST:')
-chatter(f'priv_key_to_account(BTCTEST, btctest_privkey)')
-chatter(f'send_tx(BTCTEST, btctest_account, btctest_receiver, 0.000001)')
 
-# Get transaction history
-chatter(f'# Command to get BTCTEST transaction history:')
-chatter(f'btctest_account.get_transactions()')
 
-# Get balance
-chatter(f'# Command to get BTCTEST account balance:')
-chatter(f'btctest_account.get_balance("btc")')
+# # ETH Transaction
+# chatter(f'# Commands to send a transaction using ETH:')
+# chatter(f'priv_key_to_account(ETH, eth_privkey)')
+# chatter(f'send_tx(ETH, eth_account, eth_receiver, 0.000001)')
+
+# # BTC Transaction
+# chatter(f'# Commands to send a transaction using BTCTEST:')
+# chatter(f'priv_key_to_account(BTCTEST, btctest_privkey)')
+# chatter(f'send_tx(BTCTEST, btctest_account, btctest_receiver, 0.000001)')
+
+# # Get transaction history
+# chatter(f'# Command to get BTCTEST transaction history:')
+# chatter(f'btctest_account.get_transactions()')
+
+# # Get balance
+# chatter(f'# Command to get BTCTEST account balance:')
+# chatter(f'btctest_account.get_balance("btc")')
 
